d36dd1bb2cfadd242f07d7c0632807858923fc8b
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-24 22:50:17 +0545

    fixed settings

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -92,18 +92,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-from decouple import config
+# from decouple import config
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config('MYSQLDATABASE'),
+#         'USER': config('MYSQLUSER'),
+#         'PASSWORD': config('MYSQLPASSWORD'),
+#         'HOST': config('MYSQLHOST'),
+#         'PORT': config('MYSQLPORT'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('MYSQLDATABASE'),
-        'USER': config('MYSQLUSER'),
-        'PASSWORD': config('MYSQLPASSWORD'),
-        'HOST': config('MYSQLHOST'),
-        'PORT': config('MYSQLPORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
