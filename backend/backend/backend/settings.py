cda1417de70517e8cd750eecba753e59d17b6066
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-25 11:39:57 +0545

    update

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -26,11 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pq4-+f0&&ui*8t_c+^-y7b%^6o3j2*brf2pd3p(w&7d+$c$s-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG','False')=='True'
 
-ALLOWED_HOSTS = ["*"]
-
-
+ALLOWED_HOSTS = [
+    'house-worker-booking.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 # Application definition
 
 INSTALLED_APPS = [
